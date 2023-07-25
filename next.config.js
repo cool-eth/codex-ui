0f6151478da6f11bc0a6586b94347879cda87d73
cool-eth <icpigci@gmail.com>
2023-07-24 22:59:22 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: true,
-      },
-    ];
-  },
-};
+}
 
-module.exports = withBundleAnalyzer(nextConfig);
-// module.exports = nextConfig;
+module.exports = nextConfig
