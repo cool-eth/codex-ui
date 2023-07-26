8f24fbb36596cc08cb06ce86bbfa4f64c4043cf4
cool-eth <icpigci@gmail.com>
2023-07-25 22:14:57 -0400

    update cdx lock page header

--- pages/lock/index.tsx
+++ pages/lock/index.tsx
@@ -3,18 +3,38 @@ import Home from "@/components/home";
 import Section from "@/components/section";
 import CdxLockTabs from "./CdxLockTabs";
 import CdxLocksView from "./CdxLocksView";
+import CodexTabs from "@/components/01-atoms/tabs/Tabs";
+import { useState } from "react";
 
 export default function Lock() {
+  const [index, setIndex] = useState(0);
+
   return (
     <Home>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Section
-            header={<h3 className="text-md">CDX</h3>}
-            body={<CdxLockTabs />}
+      <div className="m-4">
+        <div>
+          <div className="flex justify-between mb-6">
+            <div>
+              <h1 className="font-bold text-2xl">CDX Lock</h1>
+              <span className="text-gray-400">
+                CDX holders can lock their CDX to vote for the gauge weight.
+              </span>
+            </div>
+          </div>
+        </div>
+        <div style={{ width: "220px" }}>
+          <CodexTabs
+            index={index}
+            setIndex={setIndex}
+            items={["Lock CDX", "View Pool Info"]}
           />
+        </div>
+      </div>
+      <Grid container>
+        <Grid item md={12} lg={8}>
+          {index == 0 && <CdxLockTabs />}
         </Grid>
-        <Grid item xs={12}>
+        <Grid item md={12} lg={4} className="w-[70%]">
           <CdxLocksView />
         </Grid>
       </Grid>
